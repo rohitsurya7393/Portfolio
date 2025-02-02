8df3e296ecb30ab3d77a5fb220950cc12a4b8ad1
rohitsurya7393 <rohitsurya7393@gmail.com>
2025-02-02 12:15:24 -0500

    commit for the projects page

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
+
+@app.route('/project/<int:project_id>')
+def project_page(project_id):
+    return render_template('project.html', project_id=project_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
